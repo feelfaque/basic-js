b59497b656aeb965ae2ad6326252ae771d9366fe
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2021-03-09 19:56:19 +0300

    fix: change code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,7 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-function dateSample(sampleActivity) {
+module.exports = function dateSample(sampleActivity) {
   if (typeof sampleActivity !== "string") {
     return false;
   } else {
