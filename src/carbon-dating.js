4c001b1a17e4361a4b1b29b3829eae22bd75288f
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2021-03-09 19:51:06 +0300

    fix: delete unnecessary code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,7 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(sampleActivity) {
+function dateSample(sampleActivity) {
   if (typeof sampleActivity !== "string") {
     return false;
   } else {
